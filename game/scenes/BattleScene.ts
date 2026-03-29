c0542b336f3f487c71d40ca61fbb2a554ac1725c
David Pantera <panterad@stanford.edu>
2026-03-28 20:54:55 -0700

    Arrow bottom at -20 (top at -48)

--- game/scenes/BattleScene.ts
+++ game/scenes/BattleScene.ts
@@ -309,7 +309,7 @@ export class BattleScene extends Phaser.Scene {
 
     // "↕ ENTER" 9px bottom-left
     // Arrow icon (big) + ENTER text (normal)
-    this.add.text(qPanelX + 4, menuY + menuH - 20, '↕', {
+    this.add.text(qPanelX + 4, menuY + menuH - 48, '↕', {
       fontFamily: 'monospace',
       fontSize: '28px',
       color: '#111111',
